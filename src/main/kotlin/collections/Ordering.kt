04efa122371a1f71628cecc4489dcf7370289e0a
alexrnov <a.novopashin5@gmail.com>
2019-09-13 09:02:33 +0900

    complete Ordering.kt

--- src/main/kotlin/collections/Ordering.kt
+++ src/main/kotlin/collections/Ordering.kt
@@ -6,6 +6,10 @@ object OrderingSample {
   fun main(args: Array<String>) {
     comparable()
     comparator()
+    natureOrder()
+    customOrders()
+    reverseOrder()
+    randomOrder()
   }
 
   private fun comparable() {
@@ -15,9 +19,68 @@ object OrderingSample {
     println("------------------------")
   }
 
+  // использование компаратора для сортировки
   private fun comparator() {
     println("comparator(): ")
-    println("new line1")
+    val comparator = Comparator {
+      str1: String, str2: String -> str1.length - str2.length
+    }
+    // сортировка по длине слов
+    println(listOf("aaa", "bb", "c").sortedWith(comparator))
+    // тот же результат
+    println(listOf("aaa", "bb", "c").sortedBy { it.length } )
+    // тот же результат
+    println(listOf("aaa", "bb", "c").sortedWith( compareBy { it.length}))
+    println("------------------------")
+  }
+
+  // стандартные функции сортировки
+  private fun natureOrder() {
+    println("natureOrder(): ")
+    val numbers = listOf("one", "two", "three", "four")
+    println(numbers.sorted()) // сортировка
+    println(numbers.sortedDescending()) // обратная сортировка
+    println("------------------------")
+  }
+
+  // настраиваемая сортировка
+  private fun customOrders() {
+    println("customOrders(): ")
+    val numbers = listOf("one", "two", "three", "four")
+    // на выходе будет коллекция только для чтения
+    val sortedNumbers = numbers.sortedBy { it.length }
+    println("sortedNumbers = $sortedNumbers")
+    // обратная сортировка по последнему символу в слове
+    val sortedByLast = numbers.sortedByDescending { it.last() }
+    println("sortedByLast = $sortedByLast")
+    println("-")
+    // настраиваемая сортировка с компаратором
+    println(numbers.sortedWith(compareBy {it.length}))
+    println("------------------------")
+  }
+
+  private fun reverseOrder() {
+    println("reverseOrder(): ")
+    val numbers = mutableListOf("one", "two", "three", "four")
+    // возвращает новую коллекцию с копиями всех элементов
+    println(numbers.reversed()) // сортировка в обратном порядке
+    println("-")
+    // возвращает коллекцию, которая ссылается на исходную коллекцию
+    // если оригинальная коллекция изменится, изменится и отсортированная
+    // коллекция и наоборот
+    val reversedNumbers = numbers.asReversed()
+    numbers[0] = "dsd" // изменение оригинальной коллекции
+    println(reversedNumbers)
+    reversedNumbers[1] ="dff" // изменение отсортированной коллекции
+    println(numbers)
+    println("------------------------")
+  }
+
+  // сортировка в случайном порядке
+  private fun randomOrder() {
+    println("randomOrder(): ")
+    val numbers = listOf("one", "two", "three", "four")
+    println(numbers.shuffled())
     println("------------------------")
   }
 }
