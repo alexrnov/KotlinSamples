3d3c04e78963f9a7700457406a6694e2ff448be7
alexrnov <a.novopashin5@gmail.com>
2019-09-13 09:16:08 +0900

    add code to AggregateOperations.kt

--- src/main/kotlin/collections/AggregateOperations.kt
+++ src/main/kotlin/collections/AggregateOperations.kt
@@ -4,6 +4,17 @@ object AggregateOperationsSamples {
 
   @JvmStatic
   fun main(args: Array<String>) {
+    operations()
+  }
 
+  private fun operations() {
+    println("operations(): ")
+    val list = listOf(1, 5, 3 ,6)
+    println("count = ${list.count()}")
+    println("max = ${list.max()}")
+    println("min = ${list.min()}")
+    println("average = ${list.average()}")
+    println("sum = ${list.sum()}")
+    println("----------------------------")
   }
 }
