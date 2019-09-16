cb455e1931d9010b384b286d73c175c20d4c03e7
alexrnov <a.novopashin5@gmail.com>
2019-09-16 09:58:41 +0900

    add code to ListSpecificOperations.kt

--- src/main/kotlin/collections/ListSpecificOperations.kt
+++ src/main/kotlin/collections/ListSpecificOperations.kt
@@ -6,6 +6,8 @@ object ListSpecificOperations {
   fun main(args: Array<String>) {
     retrievingElementsByIndex()
     retrievingListParts()
+    findingElementPositions()
+    binarySearchInSortedList()
   }
 
   private fun retrievingElementsByIndex() {
@@ -26,9 +28,38 @@ object ListSpecificOperations {
     val numbers = (0..13).toMutableList()
     val numbers2 = numbers.subList(3, 6)
     println(numbers2)
-    // изменения в полученной коллекции повлияют и на исходную коллекцию
-    numbers2.add(100)  
+    // изменения в полученной коллекции повлияют и на исходную
+    // коллекцию
+    numbers2.add(100)
     println(numbers)
+    println("------------------------")
+  }
+
+  private fun findingElementPositions() {
+    println("findingElementPositions()")
+    val numbers = listOf(1, 2, 3, 4, 2, 5)
+    println(numbers.indexOf(2)) // индекс первого элемента 2
+    println(numbers.lastIndexOf(2)) // индекс последнего элемента 2
+    println(numbers.indexOf(10)) // если нет такого элемента, вернет -1
+    println("-")
+    val numbers2 = listOf(1, 2, 3, 4)
+    // индекс первого элемента, большего чем 2
+    println(numbers2.indexOfFirst { it > 2 })
+    // индекс последнего элемента, остаток деления которого на 2 будет равен единице
+    println(numbers2.indexOfLast {it % 2 == 1} )
+    println("------------------------")
+  }
+
+  // бинарный поиск
+  private fun binarySearchInSortedList() {
+    println("binarySearchInSortedList()")
+    val numbers = mutableListOf("one", "two", "three", "four")
+    numbers.sort() // коллекция должна быть отсортирована
+    println(numbers)
+    println(numbers.binarySearch("two"))
+    println(numbers.binarySearch("a"))
+    println(numbers.binarySearch("z"))
+    println(numbers.binarySearch("two", 0, 2)) // поиск в диапазоне
     println("------------------------")
   }
 }
