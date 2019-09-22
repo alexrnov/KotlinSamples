39823fe620dff4e07fe36b1a05196e25755d85dc
alexrnov <a.novopashin5@gmail.com>
2019-09-22 09:48:46 +0900

    add code to ThisExpressions.kt

--- src/main/kotlin/other/ThisExpressions.kt
+++ src/main/kotlin/other/ThisExpressions.kt
@@ -15,10 +15,13 @@ class ExpA {
     private fun Int.foo() {
       println("foo()")
       val a = this@ExpA // this ссылается на класс ExpA
+      println("a = $a")
       val b = this@ExpB // this ссылается на класс ExpB
-      val c = this // this ссылается на приемник foo(), как Int
-      val c1 = this@foo // this ссылается на приемник foo(), как Int
-
+      println("b = $b")
+      val c = this // this ссылается на приемник foo(), т.е. на число Int
+      println("c = $c")
+      val c1 = this@foo // this ссылается на приемник foo(), т.е. на число Int
+      println("c1 = $c1")
       val funLit = lambda@ fun String.() {
         val d = this
       }
