75e4f6c26b201f27af3265bfe9b5a223871d43cd
alexrnov <a.novopashin5@gmail.com>
2019-09-22 11:06:30 +0900

    complete ThisExpressions.kt

--- src/main/kotlin/other/ThisExpressions.kt
+++ src/main/kotlin/other/ThisExpressions.kt
@@ -10,6 +10,9 @@ object ThisExpressionsSamples {
 }
 
 class ExpA {
+  fun f(l: (s: String) -> Unit) {
+    l.invoke("5")
+  }
   inner class ExpB {
     var r = 7.foo()
     private fun Int.foo() {
@@ -18,17 +21,20 @@ class ExpA {
       println("a = $a")
       val b = this@ExpB // this ссылается на класс ExpB
       println("b = $b")
-      val c = this // this ссылается на приемник foo(), т.е. на число Int
+      val c = this // this ссылается на приемник foo(), т.е. на число Int (7)
       println("c = $c")
-      val c1 = this@foo // this ссылается на приемник foo(), т.е. на число Int
+      val c1 = this@foo // this ссылается на приемник foo(), т.е. на число Int (7)
       println("c1 = $c1")
-      val funLit = lambda@ fun String.() {
-        val d = this
+      val funLit1 = lambda@ fun String.() {
+        val d = this // ссылается на значение, переданное лямде (5)
+        println("funLit1: d = $d")
       }
-
+      f(funLit1)
       val funLit2 = { s: String ->
-        val d1 = this
+        val d1 = this // ссылается на приемник foo(), т.е. на число Int (7)
+        println("funLit2: d1 = $d1")
       }
+      f(funLit2)
     }
 
   }
