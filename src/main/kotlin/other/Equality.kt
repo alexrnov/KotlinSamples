2ba0919627b5dce85e426d3633ad93478c269b6d
alexrnov <a.novopashin5@gmail.com>
2019-09-22 11:26:55 +0900

    complete Equality.kt

---
+++ src/main/kotlin/other/Equality.kt
@@ -0,0 +1,33 @@
+package other
+
+object EqualitySamples {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val e1 = ClassE("a", "b")
+    val e2 = ClassE("a", "c")
+    if (e1.equals(e2)) println("Объекты равны")
+    // переопределение метода equals влияет и на оператор ==
+    if (e1 == e2) println("Объекты равны")
+    if (e1.hashCode() == e2.hashCode()) {
+      println("хеш-коды равны")
+    }
+    // проверка ссылок на равенство
+    if (e1 !== e2) { println("Объекты ссылаются на разные адреса в памяти") }
+    val e3 = e2
+    if (e3 === e2) { println("Объекты ссылаются на один адрес памяти") }
+  }
+}
+
+class ClassE(val v1: String, val v2: String) {
+  // переопределение метода equals
+  override operator fun equals(other: Any?): Boolean {
+    if (other !is ClassE) return false
+    return this.v1 == other.v1
+  }
+
+  // переопределение метода hashCode()
+  override fun hashCode(): Int {
+    return v1.hashCode()
+  }
+}
