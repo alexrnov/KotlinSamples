c311bc5ab51c8cc936baf5e2df678b4d3d6dd6b5
alexrnov <a.novopashin5@gmail.com>
2019-09-25 08:58:29 +0900

    add code to NullSafety.kt

--- src/main/kotlin/other/NullSafety.kt
+++ src/main/kotlin/other/NullSafety.kt
@@ -4,16 +4,31 @@ object NullSafetySamples {
   @JvmStatic
   fun main(args: Array<String>) {
     checkingForNull()
+    safeCalls()
   }
 
   private fun checkingForNull() {
-    println("checkingForNull()")
+    println("checkingForNull(): ")
     var a: String = "abc"
     // a = null // ошибка компиляции
     var b: String? = "abc"
     b = null
     val l = if (b != null) b.length else -1 //  явная проверка на null
-    
+    if (b != null && b.length > 0) println("Непустая строка") else println("Пустая строка")
+    println("--------------------")
+  }
+
+  // безопасные вызовы полезны в цепочках вызовов
+  private fun safeCalls() {
+    println("safeCalls(): ")
+    val a = "Kotlin"
+    val b: String? = null
+    println(a?.length)
+    println(b?.length) // безопасный вызов
+    val listWithNulls: List<String?> = listOf("Kotlin", null)
+    for (item in listWithNulls) {
+      item?. let { println(it) } // выполнить операция для не null значений
+    }
     println("--------------------")
   }
 }
