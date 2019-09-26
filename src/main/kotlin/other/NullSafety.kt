fa35350d5f0eaa226e187190a6144198faa337aa
alexrnov <a.novopashin5@gmail.com>
2019-09-26 10:22:03 +0900

    add code to NullSafety.kt

--- src/main/kotlin/other/NullSafety.kt
+++ src/main/kotlin/other/NullSafety.kt
@@ -5,6 +5,7 @@ object NullSafetySamples {
   fun main(args: Array<String>) {
     checkingForNull()
     safeCalls()
+    elvisOperator()
   }
 
   private fun checkingForNull() {
@@ -31,4 +32,33 @@ object NullSafetySamples {
     }
     println("--------------------")
   }
+
+  private fun elvisOperator() {
+    println("elvisOperator(): ")
+    var b: String? = null
+    val l1: Int = if (b != null) b.length else -1
+    // элвис-выражение: аналогично выражению выше
+    val l2 = b?.length ?: -1
+    println("l1 = $l1, l2 = $l2")
+    println("-")
+    // использование throw и return в элвис-выражении
+    // Это может быть полезно при проверке аргументов в функции
+    fun f(o: ClassNS): String? {
+      val p1 = o.p1 ?: return null
+      val p2 = o.p2 ?: throw IllegalArgumentException("p2 expected")
+      return "$p1 $p2"
+    }
+    var s = f(ClassNS(null, null))
+    println("s = $s")
+    try {
+      s = f(ClassNS("", null))
+    } catch (e: IllegalArgumentException) {
+      println("exception: s = $s")
+    }
+    s = f(ClassNS("s1", "s2"))
+    println("s = $s")
+    println("--------------------")
+  }
 }
+
+data class ClassNS(val p1: String?, val p2: String?)
