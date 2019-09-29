a358f700eecfc29fd8a6a32530dc4699d9a23991
alexrnov <a.novopashin5@gmail.com>
2019-09-29 09:46:02 +0900

    add code to Annotations.kt

--- src/main/kotlin/other/Annotations.kt
+++ src/main/kotlin/other/Annotations.kt
@@ -1,11 +1,15 @@
 package other
 
+import kotlin.reflect.KClass
+
 object AnnotationsSamples {
 
   @JvmStatic
   fun main(args: Array<String>) {
     usage()
     constructors()
+    lambdas()
+    useSiteTarget()
   }
 
   private fun usage() {
@@ -22,7 +26,39 @@ object AnnotationsSamples {
 
     // если аннотация используется в качестве параметра другой
     // аннотации, ее имя не имеет префикс @
-    @Ann4("", ReplaceWith("")) class ClassForAn2()
+    @Ann4("", Ann3("")) class ClassForAn2()
+
+    // если необходимо указать класс в качестве аргумента аннотации,
+    // используйте класс Kotlin (KClass). Компилятор Kotlin автоматически
+    // преобразует его в класс Java, так что код Java сможет нормально
+    // видеть аннотации и аргументы.
+    @Ann5(String::class, Int::class) class ClassForAn3()
+    println("----------------------")
+  }
+
+  // аннотации также могут использоваться для лямбд
+  private fun lambdas() {
+    println("lambdas(): ")
+    // они будут применены к методу invoke (), в который генерируется
+    // тело лямбды. Это полезно для таких платформ, как Quasar,
+    // которые используют аннотации для управления параллелизмом.
+    val f = @Ann6 { println("lambda") }
+    println("----------------------")
+  }
+
+  // при аннотировании свойства или параметров главного конструктора
+  // существует несколько элементов Java, которые генерируются из
+  // соответствующего элемента Kotlin, и, следовательно, может
+  // существовать несколько возможных расположений для аннотации
+  // в сгенерированном байт-коде Java. Чтобы указать, как именно должна
+  // быть создана аннотация, используйте следующий синтаксис:
+  private fun useSiteTarget() {
+    println("useSiteTarget(): ")
+    /*
+    class ClassForAnn4(@field:Ann6 val f,
+                       @get:Ann6 val bar,
+                       @param:Ann6 val quux)
+   */
     println("----------------------")
   }
 }
@@ -62,6 +98,13 @@ annotation class Ann1
 annotation class Ann2(val why: String)
 
 annotation class Ann3(val expression: String)
-
 annotation class Ann4(val message: String,
-                            val replaceWith: ReplaceWith = ReplaceWith(""))
+                            val ann3: Ann3 = Ann3(""))
+
+annotation class Ann5(val arg1: KClass<*>, val arg2: KClass<out Any>)
+
+annotation class Ann6
+
+
+
+
