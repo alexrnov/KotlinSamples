7ea5dd58c1602edab2f744b66a365442a34aac89
alexrnov <a.novopashin5@gmail.com>
2019-10-19 08:28:22 +0900

    add code to TypeSafeBuilders.kt

--- src/main/kotlin/other/TypeSafeBuilders.kt
+++ src/main/kotlin/other/TypeSafeBuilders.kt
@@ -1,8 +1,17 @@
 package other
 
+
 object TypeSafeBuildersSamples {
   @JvmStatic
   fun main(args: Array<String>) {
 
   }
+}
+
+// Html - это фактически вызов функции, которая принимает
+// лямбда-выражение в качестве аргумента.
+fun html(init: HTML.() -> Unit): HTML {
+  val html = HTML()
+  html.init()
+  return html
 }
