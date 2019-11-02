274315994153e4c0607c1b9e2549858ee6cdd022
alexrnov <a.novopashin5@gmail.com>
2019-11-03 08:39:51 +0900

    add code to TypeSafeBuilders.kt

--- src/main/kotlin/other/TypeSafeBuilders.kt
+++ src/main/kotlin/other/TypeSafeBuilders.kt
@@ -1,7 +1,6 @@
 package other
 
-
-
+val children = ArrayList<String>()
 object TypeSafeBuildersSamples {
 
   @JvmStatic
@@ -19,28 +18,44 @@ object TypeSafeBuildersSamples {
     // которая передается как аргумент (в нашем примере это приводит к вызову
     // head и body экземпляра HTML), и затем возвращает этот экземпляр
     html {
-      this.head {}
-      this.body { }
+      head {
+        title { + "text"}
+      }
+      body {
+
+      }
+      //this.head {}
+      //this.body { }
     }
+    println(children)
 
   }
 }
 
 
 class HTML {
+
   fun head(init: Head.() -> Unit): Head {
     val head = Head()
     head.init()
     println("head")
-    //children.add(head)
+    children.add("head")
     return head
+  }
+
+  fun title(init: Title.() -> Unit): Title {
+    val title = Title()
+    title.init()
+    println("title")
+    children.add("title")
+    return title
   }
 
   fun body(init: Body.() -> Unit): Body {
     val body = Body()
     body.init()
     println("body")
-    //children.add(body)
+    children.add("body")
     return body
   }
 }
@@ -48,3 +63,9 @@ class HTML {
 class Head {}
 
 class Body {}
+
+class Title {}
+
+operator fun String.unaryPlus() {
+  children.add(this)
+}
