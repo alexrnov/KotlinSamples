d332a22a0f7fde7a8b0d9035e777dd49a08b11df
alexrnov <a.novopashin5@gmail.com>
2019-11-04 08:44:33 +0900

    add code to TypeSafeBuilders.kt

--- src/main/kotlin/other/TypeSafeBuilders.kt
+++ src/main/kotlin/other/TypeSafeBuilders.kt
@@ -27,45 +27,6 @@ object TypeSafeBuildersSamples {
       //this.head {}
       //this.body { }
     }
-    println(children)
-
   }
 }
 
-
-class HTML {
-
-  fun head(init: Head.() -> Unit): Head {
-    val head = Head()
-    head.init()
-    println("head")
-    children.add("head")
-    return head
-  }
-
-  fun title(init: Title.() -> Unit): Title {
-    val title = Title()
-    title.init()
-    println("title")
-    children.add("title")
-    return title
-  }
-
-  fun body(init: Body.() -> Unit): Body {
-    val body = Body()
-    body.init()
-    println("body")
-    children.add("body")
-    return body
-  }
-}
-
-class Head {}
-
-class Body {}
-
-class Title {}
-
-operator fun String.unaryPlus() {
-  children.add(this)
-}
