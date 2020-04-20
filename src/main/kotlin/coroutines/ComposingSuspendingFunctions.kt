4299b783e4fd560d61d5646a69a0aadc683446db
alexrnov <a.novopashin5@gmail.com>
2020-04-21 08:19:14 +0900

    composing suspending functions

--- src/main/kotlin/coroutines/ComposingSuspendingFunctions.kt
+++ src/main/kotlin/coroutines/ComposingSuspendingFunctions.kt
@@ -1,6 +1,10 @@
 package coroutines
 
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 // В этом разделе рассматриваются различные подходы
 // к составлению функций приостановки.
@@ -8,12 +12,23 @@ object SuspendingSamples {
 
   @JvmStatic
   fun main(args: Array<String>) {
-    // Последовательный по умолчанию
+    // Последовательный (по умолчанию)
     // Предположим, что у нас есть две функции приостановки, определенные
     // в другом месте, которые делают что-то полезное, например, какой-либо вызов
     // удаленного сервиса или вычисление. Предположим, что они полезны, но на
     // самом деле каждый из них просто задерживается на секунду для целей
-    // этого примера: doSomethingUsefulOne() и doSomethingUsefulTwo()
+    // этого примера: doSomethingUsefulOne() и doSomethingUsefulTwo().
+    println("f1(): ")
+    // измерить общее время, необходимое для выполнения обеих функций приостановки:
+    fun f1() = runBlocking {
+      val time = measureTimeMillis {
+        val one = doSomethingUserfulOne()
+        val two = doSomethingUsefulTwo()
+        println("The answer is ${one + two}")
+      }
+      println("time = $time")
+    }
+    f1()
   }
 
   suspend fun doSomethingUserfulOne(): Int {
